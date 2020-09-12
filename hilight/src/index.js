ba105f0a04ab41d0e2fedbf0449b4ad3b29ba62f
sujithranag <sujithranag@gmail.com>
2020-09-12 21:25:26 +0530

    commit

---
+++ hilight/src/index.js
@@ -0,0 +1,14 @@
+const express =  require('express')
+require('./db/mongoose')
+//const User = require('./models/user')
+const userRouter =  require('./routers/user')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(userRouter)
+
+app.listen(port, () => {
+    console.log('Server is up on port: '+port)
+})
