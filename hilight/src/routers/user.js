9069bb45f71156f94f2a9d5c550b3b71df291612
sujithranag <sujithranag@gmail.com>
2020-09-12 22:25:05 +0530

    Initial commit

--- hilight/src/routers/user.js
+++ hilight/src/routers/user.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
-
+ 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
     
